cd485fe86ca825a7df8f9d458a3e89f669e2ac25
root <root@OSCAR-J-ALFARO-M>
2024-01-16 15:33:22 -0500

    Iniciando con Async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,13 +2,13 @@
 """First Async and Await corroutine"""
 
 
-from asyncio import sleep
+import asyncio
 from random import uniform
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """First concurrent module"""
     i = uniform(0, max_delay)
-    await sleep(i)
+    await asyncio.sleep(i)
 
     return i
