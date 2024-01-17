0e011773dc30ba82ebda8c257216e281b2e3a529
root <root@OSCAR-J-ALFARO-M>
2024-01-16 19:26:59 -0500

    Subiendo task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env Python3
+"""First Async and Await corroutine"""
+
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """A cycle with an async function"""
+    delay = []
+    for i in range(n):
+        delay.append(await wait_random(max_delay))
+    return sorted(delay)
