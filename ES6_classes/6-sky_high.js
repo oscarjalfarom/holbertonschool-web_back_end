1294591b95762848b341d35fb117b0575efa8eb2
root <root@OSCAR-J-ALFARO-M>
2024-06-14 20:15:45 -0500

    add task 6, correction

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,5 +1,11 @@
+// Extend Buildinf
 import Building from './5-building';
 
+/**
+ * Define Class
+ *
+ * @class SkyHighBuilding
+ */
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
@@ -7,11 +13,11 @@ class SkyHighBuilding extends Building {
   }
 
   get floors() {
-    return this._floors;
+    return (this._floors);
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
 
